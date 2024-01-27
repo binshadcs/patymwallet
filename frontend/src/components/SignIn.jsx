07341c7f1bac3c5ac4342c91dbfed74b2b8c19cd
binshadcs <binshadcs@gmail.com>
2024-01-27 11:23:10 +0530

    frondend edited

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,30 +1,32 @@
+import { Link } from "react-router-dom";
+
 export function SignIn() {
     return (
-        <section class="bg-gray-50 ">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <section className="bg-gray-50 ">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       
-      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                   Sign In
               </h1>
               <h3 className="text-gray-400">
                 Enter your credential to access your account
               </h3>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                      <input type="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " placeholder="name@company.com" required="" />
+                      <label className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                      <input type="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " placeholder="name@company.com" required="" />
                   </div>
                   <div className="">
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                      <input type="password" name="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " required="" />
+                      <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                      <input type="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " required="" />
                   </div>
                   
                  
-                  <button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
-                  <p class="text-sm font-light text-gray-500 ">
-                      Already have an account? <a href="#" class="font-medium text-primary-600 hover:underline ">Sign In</a>
+                  <button type="submit" className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
+                  <p className="text-sm font-light text-gray-500 ">
+                      Already have an account? <Link to={'/signup'} className="font-medium text-primary-600 hover:underline ">Sign In</Link>
                   </p>
               </form>
           </div>
